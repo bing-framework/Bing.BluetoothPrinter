efcbd4b7cbdd9f563fec3569b6ccc8f9451e0e86
jian玄冰 <jianxuanhuo1@126.com>
2019-10-30 20:49:23 +0800

    init: 初始化项目

---
+++ src/Bing.BluetoothPrinter/Bing/BluetoothPrinter/Abstractions/IBluetoothPrinterProtocol.cs
@@ -0,0 +1,10 @@
+﻿namespace Bing.BluetoothPrinter.Abstractions
+{
+    /// <summary>
+    /// 蓝牙打印机协议。
+    /// 参考地址：https://github.com/vinint/Android-BluetoothPrinter
+    /// </summary>
+    public interface IBluetoothPrinterProtocol
+    {
+    }
+}
