7166d29ada8d7df8c8beeaa21093b1483c432a5f
jian玄冰 <jianxuanhuo1@126.com>
2019-10-31 11:39:25 +0800

    feat: 定义蓝牙打印机协议

--- src/Bing.BluetoothPrinter/Bing/BluetoothPrinter/Abstractions/IBluetoothPrinterProtocol.cs
+++ src/Bing.BluetoothPrinter/Bing/BluetoothPrinter/Abstractions/IBluetoothPrinterProtocol.cs
@@ -1,4 +1,6 @@
-﻿namespace Bing.BluetoothPrinter.Abstractions
+﻿using System.Drawing;
+
+namespace Bing.BluetoothPrinter.Abstractions
 {
     /// <summary>
     /// 蓝牙打印机协议。
@@ -6,5 +8,140 @@
     /// </summary>
     public interface IBluetoothPrinterProtocol
     {
+        /// <summary>
+        /// 设置打印纸张大小、旋转角度
+        /// </summary>
+        /// <param name="width">宽度。单位：像素(Pixcls)</param>
+        /// <param name="height">高度。单位：像素(Pixcls)</param>
+        /// <param name="orientation">打印方向</param>
+        IBluetoothPrinterProtocol SetPage(int width, int height, PrintOrientation orientation);
+
+        /// <summary>
+        /// 绘制线条
+        /// </summary>
+        /// <param name="startX">线条起始点x坐标</param>
+        /// <param name="startY">线条起始点y坐标</param>
+        /// <param name="endX">线条结束点x坐标</param>
+        /// <param name="endY">线条结束点y坐标</param>
+        /// <param name="lineWidth">线条宽度</param>
+        /// <param name="lineStyle">线条样式</param>
+        IBluetoothPrinterProtocol DrawLine(int startX, int startY, int endX, int endY, int lineWidth, LineStyle lineStyle);
+
+        /// <summary>
+        /// 画矩形
+        /// </summary>
+        /// <param name="leftTopX">矩形框左上角x坐标</param>
+        /// <param name="leftTopY">矩形框左上角y坐标</param>
+        /// <param name="rightBottomX">矩形框右下角x坐标</param>
+        /// <param name="rightBottomY">矩形框右下角y坐标</param>
+        /// <param name="lineWidth">线条宽度</param>
+        /// <param name="lineStyle">线条样式</param>
+        IBluetoothPrinterProtocol DrawRect(int leftTopX, int leftTopY, int rightBottomX, int rightBottomY,
+            int lineWidth, LineStyle lineStyle);
+
+        /// <summary>
+        /// 画文字
+        /// </summary>
+        /// <param name="startX">文字起始x坐标</param>
+        /// <param name="startY">文字起始y坐标</param>
+        /// <param name="width">文字绘制区域宽度(可以为0，不为0的时候文字需要根据宽度自动换行)</param>
+        /// <param name="height">文字绘制区域高度(可以为0)</param>
+        /// <param name="text">内容</param>
+        /// <param name="fontSize">字体大小</param>
+        /// <param name="textStyle">字体样式</param>
+        /// <param name="color">文字颜色</param>
+        /// <param name="rotation">旋转角度</param>
+        IBluetoothPrinterProtocol DrawText(int startX, int startY, int width, int height, string text, int fontSize,
+            int textStyle, int color, int rotation);
+
+        /// <summary>
+        /// 画文字
+        /// </summary>
+        /// <param name="startX">文字起始x坐标</param>
+        /// <param name="startY">文字起始y坐标</param>
+        /// <param name="width">文字绘制区域宽度(可以为0，不为0的时候文字需要根据宽度自动换行)</param>
+        /// <param name="height">文字绘制区域高度(可以为0)</param>
+        /// <param name="text">内容</param>
+        /// <param name="fontSize">字体大小</param>
+        /// <param name="textStyle">字体样式</param>
+        /// <param name="color">文字颜色</param>
+        /// <param name="rotation">旋转角度</param>
+        IBluetoothPrinterProtocol DrawText(int startX, int startY, int width, int height, string text, FontSize fontSize,
+            TextStyle textStyle, PrintColor color, RotationAngle rotation);
+
+        /// <summary>
+        /// 打印条码
+        /// </summary>
+        /// <param name="startX">条码起始x坐标</param>
+        /// <param name="startY">条码起始y坐标</param>
+        /// <param name="height">条码高度</param>
+        /// <param name="lineWidth">条码鲜甜宽度</param>
+        /// <param name="text">条码内容</param>
+        /// <param name="type">条码类型</param>
+        /// <param name="rotation">旋转角度</param>
+        IBluetoothPrinterProtocol DrawBarcode(int startX, int startY, int height, int lineWidth, string text, int type,
+            int rotation);
+
+        /// <summary>
+        /// 打印条码
+        /// </summary>
+        /// <param name="startX">条码起始x坐标</param>
+        /// <param name="startY">条码起始y坐标</param>
+        /// <param name="height">条码高度</param>
+        /// <param name="lineWidth">条码线条宽度</param>
+        /// <param name="text">条码内容</param>
+        /// <param name="type">条码类型</param>
+        /// <param name="rotation">旋转角度</param>
+        IBluetoothPrinterProtocol DrawBarcode(int startX, int startY, int height, int lineWidth, string text, BarcodeType type,
+            RotationAngle rotation);
+
+        /// <summary>
+        /// 打印二维码
+        /// </summary>
+        /// <param name="startX">二维码起始x坐标</param>
+        /// <param name="startY">二维码起始y坐标</param>
+        /// <param name="text">二维码内容</param>
+        /// <param name="unitWidth">模块的单位宽度。(1-32)</param>
+        /// <param name="level">纠错级别</param>
+        /// <param name="rotation">旋转角度</param>
+        IBluetoothPrinterProtocol DrawQrCode(int startX, int startY, string text, int unitWidth, int level,
+            int rotation);
+
+        /// <summary>
+        /// 打印二维码
+        /// </summary>
+        /// <param name="startX">二维码起始x坐标</param>
+        /// <param name="startY">二维码起始y坐标</param>
+        /// <param name="text">二维码内容</param>
+        /// <param name="unitWidth">模块的单位宽度。(1-32)</param>
+        /// <param name="level">纠错级别</param>
+        /// <param name="rotation">旋转角度</param>
+        IBluetoothPrinterProtocol DrawQrCode(int startX, int startY, string text, QrCodeUnitSize unitWidth, QrCodeCorrectionLevel level,
+            RotationAngle rotation);
+
+        /// <summary>
+        /// 打印图片
+        /// </summary>
+        /// <param name="startX">图片起始x坐标</param>
+        /// <param name="startY">图片起始y坐标</param>
+        /// <param name="bitmap">图片数据</param>
+        /// <param name="width">图片打印宽度(若宽度或高度为0，则取图片宽高，不为0，则自己缩放)</param>
+        /// <param name="height">图片打印高度(若宽度或高度为0，则取图片宽高，不为0，则自己缩放)</param>
+        IBluetoothPrinterProtocol DrawImage(int startX, int startY, Bitmap bitmap, int width, int height);
+
+        /// <summary>
+        /// 下一个标签
+        /// </summary>
+        IBluetoothPrinterProtocol FeedToNextLabel();
+
+        /// <summary>
+        /// 打印
+        /// </summary>
+        void Print();
+
+        /// <summary>
+        /// 打印并进纸
+        /// </summary>
+        void PrintAndFeed();
     }
 }
---
+++ src/Bing.BluetoothPrinter/Bing/BluetoothPrinter/Enum.cs
@@ -0,0 +1,361 @@
+﻿namespace Bing.BluetoothPrinter
+{
+    /// <summary>
+    /// 打印方向
+    /// </summary>
+    public enum PrintOrientation
+    {
+        /// <summary>
+        /// 正常
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 下
+        /// </summary>
+        Down = 1,
+        /// <summary>
+        /// 左
+        /// </summary>
+        Left = 2,
+        /// <summary>
+        /// 右
+        /// </summary>
+        Right = 3
+    }
+
+    /// <summary>
+    /// 打印颜色
+    /// </summary>
+    public enum PrintColor
+    {
+        /// <summary>
+        /// 黑字白底
+        /// </summary>
+        Black = 0,
+        /// <summary>
+        /// 白字黑底
+        /// </summary>
+        White = 1
+    }
+
+    /// <summary>
+    /// 线条样式
+    /// </summary>
+    public enum LineStyle
+    {
+        /// <summary>
+        /// 实线
+        /// </summary>
+        Full = 0,
+        /// <summary>
+        /// 虚线
+        /// </summary>
+        Dotted = 1
+    }
+
+    /// <summary>
+    /// 文字样式
+    /// </summary>
+    public enum TextStyle
+    {
+        /// <summary>
+        /// 正常
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 粗体
+        /// </summary>
+        Bold = 1,
+        /// <summary>
+        /// 斜体
+        /// </summary>
+        Italic = 2,
+        /// <summary>
+        /// 下划线
+        /// </summary>
+        Underline = 4
+    }
+
+    /// <summary>
+    /// 字体大小
+    /// </summary>
+    public enum FontSize
+    {
+        /// <summary>
+        /// 16号字体
+        /// </summary>
+        Size16 = 16,
+        /// <summary>
+        /// 20号字体
+        /// </summary>
+        Size20 = 20,
+        /// <summary>
+        /// 24号字体
+        /// </summary>
+        Size24 = 24,
+        /// <summary>
+        /// 28号字体
+        /// </summary>
+        Size28 = 28,
+        /// <summary>
+        /// 32号字体
+        /// </summary>
+        Size32 = 32,
+        /// <summary>
+        /// 40号字体
+        /// </summary>
+        Size40 = 40,
+        /// <summary>
+        /// 48号字体
+        /// </summary>
+        Size48 = 48,
+        /// <summary>
+        /// 56号字体
+        /// </summary>
+        Size56 = 56,
+        /// <summary>
+        /// 64号字体
+        /// </summary>
+        Size64 = 64,
+        /// <summary>
+        /// 72号字体
+        /// </summary>
+        Size72 = 72,
+        /// <summary>
+        /// 84号字体
+        /// </summary>
+        Size84 = 84,
+        /// <summary>
+        /// 96号字体
+        /// </summary>
+        Size96 = 96,
+        /// <summary>
+        /// 128号字体
+        /// </summary>
+        Size128 = 128,
+    }
+
+    /// <summary>
+    /// 旋转角度
+    /// </summary>
+    public enum RotationAngle
+    {
+        /// <summary>
+        /// 正常
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 旋转90°
+        /// </summary>
+        Rotate90 = 90,
+        /// <summary>
+        /// 旋转180°
+        /// </summary>
+        Rotate180 = 180,
+        /// <summary>
+        /// 旋转270°
+        /// </summary>
+        Rotate270 = 270,
+    }
+
+    /// <summary>
+    /// 条码类型
+    /// </summary>
+    public enum BarcodeType
+    {
+        /// <summary>
+        /// CODE 128
+        /// </summary>
+        Code128 = 0,
+        /// <summary>
+        /// CODE 39
+        /// </summary>
+        Code39 = 1,
+        /// <summary>
+        /// CODE 93
+        /// </summary>
+        Code93 = 2,
+        /// <summary>
+        /// Codabar
+        /// </summary>
+        Codabar = 3,
+        /// <summary>
+        /// EAN-8
+        /// </summary>
+        Ean8 = 4,
+        /// <summary>
+        /// EAN-13
+        /// </summary>
+        Ean13 = 5,
+        /// <summary>
+        /// UPC-A
+        /// </summary>
+        UpcA = 6,
+        /// <summary>
+        /// UPC-E
+        /// </summary>
+        UpcE = 7,
+        /// <summary>
+        /// I2Of5
+        /// </summary>
+        I2Of5 = 8,
+    }
+
+    /// <summary>
+    /// 二维码纠错级别
+    /// </summary>
+    public enum QrCodeCorrectionLevel
+    {
+        /// <summary>
+        /// 7%的字码可被修正
+        /// </summary>
+        L = 0,
+        /// <summary>
+        /// 15%的字码可被修正
+        /// </summary>
+        M = 1,
+        /// <summary>
+        /// 25%的字码可被修正
+        /// </summary>
+        Q = 2,
+        /// <summary>
+        /// 30%的字码可被修正
+        /// </summary>
+        H = 3,
+    }
+
+    /// <summary>
+    /// 二维码单位尺寸
+    /// </summary>
+    public enum QrCodeUnitSize
+    {
+        /// <summary>
+        /// 尺寸(1 x 1)
+        /// </summary>
+        Size1 = 1,
+        /// <summary>
+        /// 尺寸(2 x 2)
+        /// </summary>
+        Size2 = 2,
+        /// <summary>
+        /// 尺寸(3 x 3)
+        /// </summary>
+        Size3 = 3,
+        /// <summary>
+        /// 尺寸(4 x 4)
+        /// </summary>
+        Size4 = 4,
+        /// <summary>
+        /// 尺寸(5 x 5)
+        /// </summary>
+        Size5 = 5,
+        /// <summary>
+        /// 尺寸(6 x 6)
+        /// </summary>
+        Size6 = 6,
+        /// <summary>
+        /// 尺寸(7 x 7)
+        /// </summary>
+        Size7 = 7,
+        /// <summary>
+        /// 尺寸(8 x 8)
+        /// </summary>
+        Size8 = 8,
+        /// <summary>
+        /// 尺寸(9 x 9)
+        /// </summary>
+        Size9 = 9,
+        /// <summary>
+        /// 尺寸(10 x 10)
+        /// </summary>
+        Size10 = 10,
+        /// <summary>
+        /// 尺寸(11 x 11)
+        /// </summary>
+        Size11 = 11,
+        /// <summary>
+        /// 尺寸(12 x 12)
+        /// </summary>
+        Size12 = 12,
+        /// <summary>
+        /// 尺寸(13 x 13)
+        /// </summary>
+        Size13 = 13,
+        /// <summary>
+        /// 尺寸(14 x 14)
+        /// </summary>
+        Size14 = 14,
+        /// <summary>
+        /// 尺寸(15 x 15)
+        /// </summary>
+        Size15 = 15,
+        /// <summary>
+        /// 尺寸(16 x 16)
+        /// </summary>
+        Size16 = 16,
+        /// <summary>
+        /// 尺寸(17 x 17)
+        /// </summary>
+        Size17 = 17,
+        /// <summary>
+        /// 尺寸(18 x 18)
+        /// </summary>
+        Size18 = 18,
+        /// <summary>
+        /// 尺寸(19 x 19)
+        /// </summary>
+        Size19 = 19,
+        /// <summary>
+        /// 尺寸(20 x 20)
+        /// </summary>
+        Size20 = 20,
+        /// <summary>
+        /// 尺寸(21 x 21)
+        /// </summary>
+        Size21 = 21,
+        /// <summary>
+        /// 尺寸(22 x 22)
+        /// </summary>
+        Size22 = 22,
+        /// <summary>
+        /// 尺寸(23 x 23)
+        /// </summary>
+        Size23 = 23,
+        /// <summary>
+        /// 尺寸(24 x 24)
+        /// </summary>
+        Size24 = 24,
+        /// <summary>
+        /// 尺寸(25 x 25)
+        /// </summary>
+        Size25 = 25,
+        /// <summary>
+        /// 尺寸(26 x 26)
+        /// </summary>
+        Size26 = 26,
+        /// <summary>
+        /// 尺寸(27 x 27)
+        /// </summary>
+        Size27 = 27,
+        /// <summary>
+        /// 尺寸(28 x 28)
+        /// </summary>
+        Size28 = 28,
+        /// <summary>
+        /// 尺寸(29 x 29)
+        /// </summary>
+        Size29 = 29,
+        /// <summary>
+        /// 尺寸(30 x 30)
+        /// </summary>
+        Size30 = 30,
+        /// <summary>
+        /// 尺寸(31 x 31)
+        /// </summary>
+        Size31 = 31,
+        /// <summary>
+        /// 尺寸(32 x 32)
+        /// </summary>
+        Size32 = 32,
+    }
+}
